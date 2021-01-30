9a7a14bce97f71da3d9c8d323fc858f344c0c59c
unknown <trong.phamtranduc@gmail.com>
2021-01-30 20:34:30 +0700

    lan 1

--- program.js
+++ program.js
@@ -1,8 +1,24 @@
 // Add new functions, variables here
 
 function main(input) {
-  // Your code goes here
-  // Using console.log to answer the question
+  let inp = input.split(' ');
+  let num = inp[0];
+  let a = inp[1];
+  let b = inp[2];
+  if(a==10 && b==2){
+    decToBi(num);
+  }
+}
+function decToBi(num) {
+  let bin = 0;
+  let rem, i = 1;
+  while (num != 0) {
+      rem = num % 2;
+      num = parseInt(num / 2);
+      bin = bin + rem * i;
+      i = i * 10;
+  }
+  console.log(bin);
 }
 
 module.exports = main;
