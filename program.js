743be3614ae40b818cceafd51b7759521d704f5c
Pham Tran Duc Trong <trong.ptd3604@sinhvien.hoasen.edu.vn>
2021-01-31 11:13:07 +0700

    lan 2

--- program.js
+++ program.js
@@ -1,24 +1,41 @@
-// Add new functions, variables here
+//Add new functions, variables here
 
 function main(input) {
-  let inp = input.split(' ');
-  let num = inp[0];
-  let a = inp[1];
-  let b = inp[2];
-  if(a==10 && b==2){
-    decToBi(num);
-  }
+    let inp = input.split(' ');
+    let num = inp[0];
+    let a = inp[1];
+    let b = inp[2];
+    if (a == 10 && b == 2) {
+        decToBi(num);
+    }
+    if (a == 2 && b == 10) {
+        biTODec(num);
+    }
 }
+
 function decToBi(num) {
-  let bin = 0;
-  let rem, i = 1;
-  while (num != 0) {
-      rem = num % 2;
-      num = parseInt(num / 2);
-      bin = bin + rem * i;
-      i = i * 10;
-  }
-  console.log(bin);
+    let bin = 0;
+    let rem, i = 1;
+    while (num != 0) {
+        rem = num % 2;
+        num = parseInt(num / 2);
+        bin = bin + rem * i;
+        i = i * 10;
+    }
+    console.log(bin);
+}
+
+function biTODec(num) {
+    let decimal = +0;
+    let bits = +1;
+    for (let i = 0; i < num.length; i++) {
+        let currNum = +(num[num.length - i - 1]);
+        if (currNum === 1) {
+            decimal += bits;
+        }
+        bits *= 2;
+    }
+    console.log(decimal);
 }
 
 module.exports = main;
